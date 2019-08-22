ae34aea91d4ea351a6fa0da0f6fbed7759970455
tiwashet <madkvns007@gmail.com>
2019-08-22 08:24:31 +0530

    using out parameter

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -6,19 +6,25 @@ namespace CSharpPlaying
     public class Programme
     {
         static void Main(string[] args)
-        {
-           string first = "Nishet";
-            string second = "Kumar";
-            Swap(ref first,ref second);
-            Console.WriteLine(first);
-            Console.WriteLine(second);
-        }
+        {            
+            string a, b;
+            Split("nishet kumar tiwari",out a,out b);
+            /////////////////////////////
+            Foo(out x);
+        }      
        
-        static void Swap(ref string first, ref string second)
+        static void Split(string name,out string firstName,out string Lastname)
         {
-            string temp = first;
-            first = second;
-            second = temp;
+            int i = name.LastIndexOf(' ');
+            firstName = name.Substring(0, i);
+            Lastname = name.Substring(i + 1);
+        }
+        static int x;
+        static void Foo(out int y)
+        {
+            Console.WriteLine(  x);
+            y = 11;
+            Console.WriteLine(x);
         }
     }
 }
