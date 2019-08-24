991076b108426007a31f6dd91ff365e4cddf278d
tiwashet <madkvns007@gmail.com>
2019-08-24 12:26:00 +0530

    null coalescing and (null condtional or elvis operator)

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -6,25 +6,12 @@ namespace CSharpPlaying
     public class Programme
     {
         static void Main(string[] args)
-        {            
-            string a, b;
-            Split("nishet kumar tiwari",out a,out b);
-            /////////////////////////////
-            Foo(out x);
-        }      
-       
-        static void Split(string name,out string firstName,out string Lastname)
         {
-            int i = name.LastIndexOf(' ');
-            firstName = name.Substring(0, i);
-            Lastname = name.Substring(i + 1);
-        }
-        static int x;
-        static void Foo(out int y)
-        {
-            Console.WriteLine(  x);
-            y = 11;
-            Console.WriteLine(x);
-        }
+            StringBuilder sb = null;
+
+            string s2 = sb?.ToString() ?? "Nothing";
+
+            Console.WriteLine(s2);
+        }  
     }
 }
