dcfca74533dba87011e6ff0058ee5bf7213bfe3f
tiwashet <madkvns007@gmail.com>
2019-09-07 19:46:17 +0530

    indexer implementation

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -8,29 +8,49 @@ using static System.Console;
 
 namespace CSharpPlaying
 {
+    public class Testing
+    {
+        public string Name;
+        
+        public Testing() { }
 
+        /// <summary>
+        /// Use the static method Hello
+        /// </summary>
+        /// <param name="name"></param>
+        private Testing(string name)
+            {  }
+        public static  Testing Hello() { return new Testing(""); }
+    }
+
+    public class Panda
+    {
+        string[] words = "This is a sentence".Split();
+
+        //Indexer implimentatiion
+        public string this [int wordNum]
+        {
+            get { return words[wordNum]; }
+            set { words[wordNum] = value; }
+        }
+        public int Population {  get; private set; } 
+        public Panda Mate;
+        public void Marry(Panda partner)
+        {
+            Mate = partner;
+            partner.Mate = this;
+        }
+    }
 
     public class Programme
     {
         static void Main(string[] args)
         {
-
-            FibonacciSequence();
+            Panda p = new Panda();
+            p[3] = "donkey";
+            Console.WriteLine(p[3]);
         }
-        static void FibonacciSequence()
-        {
-            for (int i = 0, prevFib = 1, currFib = 1; i < 10; i++)
-            {
-
-                Console.WriteLine(currFib);
-                int newFib = currFib + prevFib;
-
-                prevFib = currFib;
-                currFib = newFib;
-
-
-            }
-        }
+        
     }
 
 }
