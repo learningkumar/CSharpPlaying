22302e05d68f3058aec63231f1c0a837b7608911
tiwashet <madkvns007@gmail.com>
2019-09-11 21:41:49 +0530

    Abstract class with abstract method overiding

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -8,29 +8,42 @@ using static System.Console;
 
 namespace CSharpPlaying
 {
-    public class Asset
+    public abstract class Asset
     {
         public string Name { get; set; }
+        public virtual decimal Liability => 0;
+
+        public abstract decimal NetValue { get; }
     }
 
-    public class Stock:Asset
+    public class Stock : Asset
     {
+        public new string Name { get; set; }
+        public decimal CurrentPrice;
         public int NumberOfShares { get; set; }
+        public override decimal NetValue => CurrentPrice * NumberOfShares;
+
     }
 
-    public class House:Asset
+    public class House : Asset
     {
+        public decimal CurrentPrice;
         public decimal MortgageAmount { get; set; }
+        public override decimal Liability => MortgageAmount;
+
+        public override decimal NetValue => CurrentPrice;
+
     }
 
     public class Programme
     {
         static void Main(string[] args)
         {
-            Stock stock = new Stock() { Name = "msft", NumberOfShares = 10 };
-            House hut = new House() { Name = "Hut", MortgageAmount = 40.9M };
+            Stock stock = new Stock() { Name = "msft", NumberOfShares = 10, CurrentPrice = 100 };
+            House hut = new House() { Name = "Hut", MortgageAmount = 40.9M, CurrentPrice = 200 };
+
             Display(stock);
-            Display(hut);
+            //Display(hut);
         }
         static void Display(Asset asset)
         {
