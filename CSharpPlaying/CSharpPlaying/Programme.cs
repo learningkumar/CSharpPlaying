c5b94de752d8432adf949f4cb1116dda5718d994
tiwashet <madkvns007@gmail.com>
2019-09-15 12:07:35 +0530

    method binding in inheritance and method overiding pattern

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -8,25 +8,45 @@ using static System.Console;
 
 namespace CSharpPlaying
 {
-    public class Base
+    public  class Base
     {
-        public virtual void Foo() { Console.WriteLine("Inside base class"); }
+        public Base()
+        {
+
+        }
+        public Base(int number)
+        {
+
+        }
+        public virtual  void Foo() { Console.WriteLine("Inside base class"); }
     }
 
     public class Overrider:Base
     {
-        public override void Foo() { Console.WriteLine("Inside Overrider class"); }
+        public Overrider()
+        {
+                
+        }
+        public  override void Foo() { Console.WriteLine("Inside Overrider class"); }
+    }
+    public class AnotherOverrider:Overrider
+    {
+        public override void Foo()
+        {
+            base.Foo();
+        }
     }
 
     public class Hider : Base
     {
-        public new void Foo() { Console.WriteLine("Inside Hider class"); }
+        public new void Foo() {Console.WriteLine("Inside Hider class"); }
     }
 
     public class Programme
     {
         static void Main(string[] args)
         {
+            
             Overrider o = new Overrider();
             Base b1 = o;
             o.Foo();
@@ -34,8 +54,26 @@ namespace CSharpPlaying
 
             Hider h = new Hider();
             Base b2 = h;
+            meth(h);
+            meth(b2);
+            meth(o);
+            meth(b1);
             h.Foo();
             b2.Foo();
+        }
+
+        static void meth(Base b)
+        {
+            b.Foo();
+        }
+
+        static void meth(Overrider b)
+        {
+            b.Foo();
+        }
+        static void meth(Hider b)
+        {
+            b.Foo();
         }
     }
 }
