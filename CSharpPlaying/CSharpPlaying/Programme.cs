3e5a7a3a90640733ae36ac4ecdd80adf204411cf
tiwashet <madkvns007@gmail.com>
2019-10-09 23:05:28 +0530

    example of multicaste delegate

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -11,6 +11,7 @@ using static System.Console;
 namespace CSharpPlaying
 {
     public delegate int Transformer(int x);
+    public delegate void ProgressReporter(int percentComplete);
     public class Util
     {
         public static void Transform(int[] values, Transformer t)
@@ -20,10 +21,24 @@ namespace CSharpPlaying
                 values[i] = t(values[i]);
             }
         }
+        public static void HardWork(ProgressReporter p)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                p(i * 10);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
     }
     public class Programme
     {
         static void Main(string[] args)
+        {
+            ProgressReporter p = WriteProgressToConsole;
+            p += WriteProgressToFile;
+            Util.HardWork(p);
+        }
+        static void Main1(string[] args)
         {
             int[] values = { 1, 2, 3 };
             Transformer t = Square;
@@ -38,6 +53,8 @@ namespace CSharpPlaying
         static int Square(int x) => x * x;
         static int Cube(int x) => x * x * x;
 
+        static void WriteProgressToConsole(int percentComplete) => Console.WriteLine(percentComplete);
+        static void WriteProgressToFile(int percentComplete) => System.IO.File.WriteAllText("progress.txt", percentComplete.ToString());
 
     }
 }
