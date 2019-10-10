c5fc4cf1159911345b61b7cac1794b1a45d12113
tiwashet <madkvns007@gmail.com>
2019-10-10 22:48:09 +0530

    Generic delegate

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -10,64 +10,30 @@ using static System.Console;
 
 namespace CSharpPlaying
 {
-    
-    public delegate int Transformer(int x);
-    public delegate void ProgressReporter(int percentComplete);
+    public delegate T Transformer<T>(T item);
     public class Util
     {
-        
-        public static void Transform(int[] values, Transformer t)
+        public static void Transform<T>(T[] values,Transformer<T> t)
         {
-            
             for (int i = 0; i < values.Length; i++)
             {
                 values[i] = t(values[i]);
             }
         }
-        public static void HardWork(ProgressReporter p)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                p(i * 10);
-                System.Threading.Thread.Sleep(100);
-            }
-        }
     }
+
     public class Programme
     {
-        class X
-        {
-            public void InstanceProgress(int percentComplete)
-            => Console.WriteLine(percentComplete);
-        }
         static void Main(string[] args)
         {
-            X x = new X();
-            ProgressReporter p1 = x.InstanceProgress;
-            p1(99); // 99
-            Console.WriteLine(p1.Target == x); // True
-            Console.WriteLine(p1.Method); // Void InstanceProgress(Int32)
-            ProgressReporter p = WriteProgressToConsole;
-            p += WriteProgressToFile;
-            Util.HardWork(p);
-        }
-        static void Main1(string[] args)
-        {
             int[] values = { 1, 2, 3 };
-            Transformer t = Square;
-            t += Cube;
-            Util.Transform(values, t);
-            foreach (int  i in values)
+            Util.Transform(values, Square);
+
+            foreach (int item in values)
             {
-                Console.WriteLine(i+" ");
+                Console.WriteLine(item);
             }
-           
         }
-        static int Square(int x) => x * x;
-        static int Cube(int x) => x * x * x;
-
-        static void WriteProgressToConsole(int percentComplete) => Console.WriteLine(percentComplete);
-        static void WriteProgressToFile(int percentComplete) => System.IO.File.WriteAllText("progress.txt", percentComplete.ToString());
-
+        public static int Square(int x) => x * x;
     }
 }
