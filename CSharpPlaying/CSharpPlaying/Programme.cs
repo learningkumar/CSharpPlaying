91a23c34232d0f0ad6db3004e253549f70e580c2
tiwashet <madkvns007@gmail.com>
2019-10-13 14:27:19 +0530

    Writing event as per windows defined standards. The information file contains detailed steps

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -34,20 +34,69 @@ namespace CSharpPlaying
                     PriceChanged(oldPrice, _price);
             }
         }
+    }
 
+    //As per windows standard one needs to define a subclass of eventargs class as a way of passing event info
+    public class PriceChangedEventArgs : EventArgs
+    {
+        public readonly decimal oldPrice;
+        public readonly decimal newPrice;
+        public PriceChangedEventArgs(decimal oldPrice, decimal newPrice)
+        {
+            this.oldPrice = oldPrice;
+            this.newPrice = newPrice;
+        }
+    }
 
+    //Windows standard way of writing event 
+    public class Equity
+    {
+        string _symbol;
+        decimal _price;
+        public Equity(string symbol)
+        {
+            _symbol = symbol;
+        }
+        public event EventHandler<PriceChangedEventArgs> PriceChanged;
+        protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+        {
+            PriceChanged?.Invoke(this, e);
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (Price == value)
+                    return;
+                decimal oldPrice = _price;
+                _price = value;
+                OnPriceChanged(new PriceChangedEventArgs(oldPrice, _price));
+            }
+        }
     }
     public class Programme
     {
         static void Main(string[] args)
         {
+            //With Custom delegate/Event
             Stock stock = new Stock("INFY");
             stock.PriceChanged += DisplayPrice;
+
             stock.Price = 10;
+
+            //With windows standard way of writing event
+            Equity equity = new Equity("TCS");
+            equity.PriceChanged += DisplayPrice;
+            equity.Price = 90;
         }
-        static void DisplayPrice(decimal oldPrice , decimal newPrice)
+        static void DisplayPrice(decimal oldPrice, decimal newPrice)
         {
             Console.WriteLine($"Old price is  : {oldPrice} \n New Price is : {newPrice}");
+        }
+        static void DisplayPrice(object o, PriceChangedEventArgs e)
+        {
+            Console.WriteLine($"Old price is  : {e.oldPrice} \n New Price is : {e.newPrice}");
         }
     }
 }
