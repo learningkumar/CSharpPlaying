759b9224b9290a03c9d9dcf33fe05b0ad98178ed
tiwashet <madkvns007@gmail.com>
2019-10-22 22:30:06 +0530

    lambda expression capturing outer variables(closures)

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -10,105 +10,29 @@ using static System.Console;
 
 namespace CSharpPlaying
 {
-    public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
-
-    public class Stock
-    {
-        string _symbol;
-        decimal _price;
-        public Stock(string Symbol)
-        {
-            _symbol = Symbol;
-        }
-        public event PriceChangedHandler PriceChanged;
-        public decimal Price
-        {
-            get { return _price; }
-            set
-            {
-                if (_price == value)
-                    return;
-                decimal oldPrice = _price;
-                _price = value;
-                if (PriceChanged != null)
-                    PriceChanged(oldPrice, _price);
-            }
-        }
-    }
-
-    //As per windows standard one needs to define a subclass of eventargs class as a way of passing event info
-    public class PriceChangedEventArgs : EventArgs
-    {
-        public readonly decimal oldPrice;
-        public readonly decimal newPrice;
-        public PriceChangedEventArgs(decimal oldPrice, decimal newPrice)
-        {
-            this.oldPrice = oldPrice;
-            this.newPrice = newPrice;
-        }
-    }
-
-    //Windows standard way of writing event 
-    public class Equity
-    {
-        string _symbol;
-        decimal _price;
-        public Equity(string symbol)
-        {
-            _symbol = symbol;
-        }       
-        public event EventHandler<PriceChangedEventArgs> PriceChanged;
-        protected virtual void OnPriceChanged(PriceChangedEventArgs e)
-        {
-            PriceChanged?.Invoke(this, e);
-        }
-        public decimal Price
-        {
-            get { return _price; }
-            set
-            {
-                if (Price == value)
-                    return;
-                decimal oldPrice = _price;
-                _price = value;
-                OnPriceChanged(new PriceChangedEventArgs(oldPrice, _price));
-            }
-        }
-    }
+    public delegate int Transformer(int i);
     public class Programme
     {
+        static Func<int> Natural()
+        {
+            int seed = 0;
+            return () => seed++;
+        }
         static void Main(string[] args)
         {
-            MyForm mf = new MyForm();
-            mf.ShowDialog();
-            
-            //With Custom delegate/Event
-            Stock stock = new Stock("INFY");
-            stock.PriceChanged += DisplayPrice;
-
-            stock.Price = 10;
-
-            //With windows standard way of writing event
-            Equity equity = new Equity("TCS");
-            equity.PriceChanged += DisplayPrice;
-            equity.Price = 90;
+            Func<int> fnn = Natural();
+            Console.WriteLine(fnn());
+            Console.WriteLine(fnn());
+            Console.WriteLine(fnn());
+            //Transformer tr = x => x * x;
+            //Func<int, int> fn = x => x * x;
+            //Func<string, string, int> fns = (s1, s2) => s1.Length + s2.Length;
+            //Console.WriteLine(fns("hello", "world"));
+            //Bar((int x) => Foo(x));
+            //Bar<int>(x => Foo(x));
+            //Bar<int>(Foo);
         }
-        static void DisplayPrice(decimal oldPrice, decimal newPrice)
-        {
-            Console.WriteLine($"Old price is  : {oldPrice} \n New Price is : {newPrice}");
-        }
-        static void DisplayPrice(object o, PriceChangedEventArgs e)
-        {
-            Console.WriteLine($"Old price is  : {e.oldPrice} \n New Price is : {e.newPrice}");
-        }
-    }
-
-    public class class1
-    {
-        public virtual event EventHandler ev;
-    }
-    public class class2:class1
-    {
-        public override event EventHandler ev;
+        static void Foo<T>(T x) { }
+        static void Bar<T>(Action<T> a) { }
     }
 }
