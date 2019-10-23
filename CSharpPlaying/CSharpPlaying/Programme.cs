ce002fc02d20b7665a9ae166316e3d67c1b372ca
tiwashet <madkvns007@gmail.com>
2019-10-23 06:45:01 +0530

    capturing loop variable in a lambda expression

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -2,6 +2,7 @@
 extern alias W2;//Using extern alias
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using static System.Console;
@@ -13,26 +14,47 @@ namespace CSharpPlaying
     public delegate int Transformer(int i);
     public class Programme
     {
-        static Func<int> Natural()
-        {
-            int seed = 0;
-            return () => seed++;
-        }
+       
         static void Main(string[] args)
         {
-            Func<int> fnn = Natural();
-            Console.WriteLine(fnn());
-            Console.WriteLine(fnn());
-            Console.WriteLine(fnn());
-            //Transformer tr = x => x * x;
-            //Func<int, int> fn = x => x * x;
-            //Func<string, string, int> fns = (s1, s2) => s1.Length + s2.Length;
-            //Console.WriteLine(fns("hello", "world"));
-            //Bar((int x) => Foo(x));
-            //Bar<int>(x => Foo(x));
-            //Bar<int>(Foo);
+            //Capturing iteration variable in a lambda expression in a for loop causes the following behaviour
+            Action[] action = new Action[3];
+            for (int j = 0; j < 3; j++)
+            {
+                action[j] = () => Console.WriteLine(j);
+            }
+            foreach (Action item in action)
+            {
+                item();//this will print 333
+            }
+
+            //Same as above but if we want some not surprising behaviour then this
+            for (int k = 0; k < 3; k++)
+            {
+                int num = k;
+                action[k] = () => Console.WriteLine(k);
+            }
+            foreach (Action item in action)
+            {
+                item();//this will print 012
+            }
+
+
+
+            //iteration variable capturing in foreach loop causes differnet behaviour than the for loop as the iterating variable in foreach loop is immutable
+            int i = 0;
+            var ran = Enumerable.Range(1, 3);
+            foreach (var item in ran)
+            {
+                action[i++] = () => Console.WriteLine(item);
+            }
+
+            foreach (var item in action)
+            {
+                item();
+            }
+          
         }
-        static void Foo<T>(T x) { }
-        static void Bar<T>(Action<T> a) { }
+       
     }
 }
