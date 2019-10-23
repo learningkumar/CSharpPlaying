42e00138678d79d5044d30065e64d3a6018f9817
tiwashet <madkvns007@gmail.com>
2019-10-23 07:25:25 +0530

    Anonymous method

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -14,45 +14,12 @@ namespace CSharpPlaying
     public delegate int Transformer(int i);
     public class Programme
     {
+        public delegate int Transformer(int num);
        
         static void Main(string[] args)
         {
-            //Capturing iteration variable in a lambda expression in a for loop causes the following behaviour
-            Action[] action = new Action[3];
-            for (int j = 0; j < 3; j++)
-            {
-                action[j] = () => Console.WriteLine(j);
-            }
-            foreach (Action item in action)
-            {
-                item();//this will print 333
-            }
-
-            //Same as above but if we want some not surprising behaviour then this
-            for (int k = 0; k < 3; k++)
-            {
-                int num = k;
-                action[k] = () => Console.WriteLine(k);
-            }
-            foreach (Action item in action)
-            {
-                item();//this will print 012
-            }
-
-
-
-            //iteration variable capturing in foreach loop causes differnet behaviour than the for loop as the iterating variable in foreach loop is immutable
-            int i = 0;
-            var ran = Enumerable.Range(1, 3);
-            foreach (var item in ran)
-            {
-                action[i++] = () => Console.WriteLine(item);
-            }
-
-            foreach (var item in action)
-            {
-                item();
-            }
+            Transformer t = delegate (int x) { return x * x; };
+            Console.WriteLine(t(8));
           
         }
        
