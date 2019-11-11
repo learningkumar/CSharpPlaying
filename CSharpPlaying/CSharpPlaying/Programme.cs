5ef8e3560e62905dba8217c614ff1a57d702df90
tiwashet <madkvns007@gmail.com>
2019-11-11 22:47:18 +0530

    Iterator examples

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -14,23 +14,49 @@ namespace CSharpPlaying
 {
     public class Programme
     {
-        
+
         static void Main(string[] args)
         {
-            foreach (int fib in Fibs(3))
+            var sequence = Foo(false);
+
+            IEnumerator<string> er = sequence.GetEnumerator();
+            while (er.MoveNext())
+                Console.WriteLine(er.Current);
+            foreach (string item in Foo(true))
             {
-                Console.WriteLine(fib);
+                Console.WriteLine(item);
+            }
+            foreach (int fib in EvenFibsOnly(Fibs(3)))
+            {
+                 Console.WriteLine(fib);
             }
         }
         static IEnumerable<int> Fibs(int FibCount)
         {
-            for (int i = 0,prevFib=1,currFib=1; i < FibCount; i++)
+            for (int i = 0, prevFib = 1, currFib = 1; i < FibCount; i++)
             {
                 yield return prevFib;
                 int newFib = prevFib + currFib;
                 prevFib = currFib;
                 currFib = newFib;
             }
+        }
+        static IEnumerable<int> EvenFibsOnly(IEnumerable<int> fibNums)
+        {
+            foreach (int item in fibNums)
+            {
+                if (item % 2 == 0)
+                    yield return item;
+            }
+        }
+        static IEnumerable<string> Foo(bool breakEarly)
+        {
+            yield return "one";
+            yield return "two";
+            if (breakEarly)
+                yield break;
+            yield return "three";
+
         }
     }
 }
