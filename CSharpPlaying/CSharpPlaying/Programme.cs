922f9d68bf27acc9d64f944ae3fb045895e9bcb5
tiwashet <madkvns007@gmail.com>
2019-11-26 06:54:48 +0530

    overloading of implicit and explicit operator

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -23,58 +23,17 @@ namespace CSharpPlaying
             value = semiTonesFromA;
         }
         public static Note operator +(Note x, int semiTones) => new Note(x.value + semiTones);
+        
+        public static implicit operator double(Note n)=> n.value+ 4.5;
+        public static explicit operator Note(double d)=>new Note((int)d);
     }
     public class Programme
     {
         static void Main(string[] args)
         {
-            //Operator overloading
-            Note note = new Note(3);
-            Note note1 = note + 4;
-
-          
-
-
-            //var sequence = Foo(false);
-
-            //IEnumerator<string> er = sequence.GetEnumerator();
-            //while (er.MoveNext())
-            //    Console.WriteLine(er.Current);
-            //foreach (string item in Foo(true))
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (int fib in EvenFibsOnly(Fibs(3)))
-            //{
-            //    Console.WriteLine(fib);
-            //}
-        }
-        static IEnumerable<int> Fibs(int FibCount)
-        {
-            for (int i = 0, prevFib = 1, currFib = 1; i < FibCount; i++)
-            {
-                yield return prevFib;
-                int newFib = prevFib + currFib;
-                prevFib = currFib;
-                currFib = newFib;
-            }
-        }
-        static IEnumerable<int> EvenFibsOnly(IEnumerable<int> fibNums)
-        {
-            foreach (int item in fibNums)
-            {
-                if (item % 2 == 0)
-                    yield return item;
-            }
-        }
-        static IEnumerable<string> Foo(bool breakEarly)
-        {
-            yield return "one";
-            yield return "two";
-            if (breakEarly)
-                yield break;
-            yield return "three";
-
+            Note n = new Note(3);
+            double d = n;
+            Note ng = (Note)d; 
         }
     }
 }
