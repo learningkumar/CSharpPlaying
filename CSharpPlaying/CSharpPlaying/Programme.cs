5cb3f3d1daf66ecddb72e4d0179d7d5bd203e12b
tiwashet <madkvns007@gmail.com>
2019-12-06 21:52:39 +0530

    attributes usage

--- CSharpPlaying/CSharpPlaying/Programme.cs
+++ CSharpPlaying/CSharpPlaying/Programme.cs
@@ -2,36 +2,74 @@
 extern alias W2;//Using extern alias
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Controls;
+using System.Xml.Serialization;
 using static System.Console;
 
 
 
 namespace CSharpPlaying
 {
+    public interface IFoo
+    {
+        void Foo();
+    }
+    public class Test:IFoo
+    {
+         void IFoo.Foo() { }
+    }
+    public class Foo : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged=delegate { };
+        void RaiseOnPropertyChanged([CallerMemberName]string propertyName=null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        string customerName;    
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                if (value == customerName) return;
+                else
+                {
+                    customerName = value;
+                    RaiseOnPropertyChanged();
+                }
+
+            }
+        }
+
+    }
+
+
 
     public class Programme
     {
+        
+        private int v;
         static void Main(string[] args)
         {
-            dynamic d = new Duck();
-            d.Quack();
-            d.Waddle();
+            Foo();
+            INotifyPropertyChanged
         }
-    }
-    public class Duck:DynamicObject
-    {
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        static void Foo([CallerMemberName]string memberName=null,[CallerFilePath]string filePath = null,[CallerLineNumber]int lineNumber=0)
         {
-            Console.WriteLine(binder.Name+" method was called");
-            result = null;
-            return true;
+            Console.WriteLine(memberName);
+            Console.WriteLine(filePath);
+            Console.WriteLine(lineNumber);
         }
+        
+        static dynamic Mean(dynamic x, dynamic y) => (x + y) / 2;
     }
- 
+
+
 }
